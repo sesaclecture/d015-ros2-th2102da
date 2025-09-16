d06410a6b9ec3bc3dcecc40244d10659843abc90
김태현 <th2102da@gmail.com>
2025-09-16 09:34:29 +0900

    과제제출

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ def task_source_ros() -> tuple[int, str, str]:
     ROS2 Jazzy 환경을 불러오는 명령어를 반환하시오.
     """
     # TODO: 여기에 코드를 작성하시오
-    cmd = ""
+    cmd = "source /opt/ros/jazzy/setup.bash"
     return _run(f"bash -lc '{cmd}'")
 
 
@@ -47,11 +47,14 @@ def task_pkg_create_cmd() -> tuple[int, str, str]:
       - 코드 설명은 D015 homework ROS2 workspace로 지정
     """
     # TODO: 여기에 코드를 작성하시오
-    workspace = Path.home()
-    src_dir = ""
+    workspace = Path.home() / "ws_ros2"
+    src_dir = workspace / "src"
     src_dir.mkdir(parents=True, exist_ok=True)
 
-    cmd = ""
+    cmd = ("ros2 pkg create ros_pkg --build-type ament_python --license MIT "
+        '--maintainer-name "kcci" --maintainer-email "kcci@kcci.com" '
+        "--dependencies rclpy std_msgs "
+        '--description "D015 homework ROS2 workspace"')
     return _run(cmd, cwd=os.path.join(workspace, "src"))
 
 
@@ -61,9 +64,9 @@ def task_colcon_build_cmd() -> tuple[int, str, str]:
     workspace(~/ws_ros2) 경로에서 ros2 빌드하는 명령어를 반환하시오.
     """
     # TODO: 여기에 코드를 작성하시오
-    workspace = Path.home() + "???"
+    workspace = Path.home() + "/ws_ros2"
     workspace.mkdir(parents=True, exist_ok=True)
-    cmd = "???"
+    cmd = "colcon build --symlink-install"
     return _run(cmd, cwd=workspace)
 
 
@@ -73,7 +76,7 @@ def task_xhost_cmd() -> tuple[int, str, str]:
     root 유저에 X 권한을 여는 xhost 명령어를 반환하시오.
     """
     # TODO: 여기에 코드를 작성하시오
-    cmd = "..."
+    cmd = "xhost +si:localuser:root"
     return _run(cmd)
 
 
@@ -93,7 +96,15 @@ def task_docker_run_cmd() -> str:
     """
     # TDO: 여기에 코드를 작성하시오
     return (
-        "docker run -it --rm --name ...."
+        "docker run -it --rm "
+        "--name humble-gui "
+        "--network host "
+        "-e DISPLAY=$DISPLAY "
+        "-e ROS_DOMAIN_ID=24 "
+        "-e RMW_IMPLEMENTATION=rmw_cyclondds_cpp "
+        "-v /tmp/.X11-unix:/tmp/.X11-unix:rw "
+        "arm64v8/ros:humble "
+        "bash"
     )
 
 
